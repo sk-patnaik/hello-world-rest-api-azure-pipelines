7341a6aac257aa66b0b9996900bd7e476d16a34e
sk-patnaik <sitakanta.patnaik@shell.com>
2021-02-20 14:58:35 +0530

    sixth commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -16,7 +16,7 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
 		// ***-***");
-		return new HelloWorldBean("Hello WorldV15");
+		return new HelloWorldBean("Hello WorldV18");
 	}
 
 	/// hello-world/path-variable/in28minutes
